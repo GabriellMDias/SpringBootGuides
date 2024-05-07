65806eb10c479e93c106aaaefe94981fd8b8f633
Gabriel Dias <gabrielmoreiradias270402@gmail.com>
2024-05-07 13:46:42 -0300

    Acessing JPA Data With REST

---
+++ jpaRest/src/main/java/com/example/jpaRest/Person.java
@@ -0,0 +1,42 @@
+package com.example.jpaRest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+
+    private String middleName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
